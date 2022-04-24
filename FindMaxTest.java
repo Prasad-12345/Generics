59f0a9d9b97910fafa63d789f01b7623e1b8a6fd
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-24 18:05:46 +0530

    Added: Test cases to check maximun number and its position

---
+++ FindMaxTest.java
@@ -0,0 +1,70 @@
+package com.bridgelabz.generics;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Scanner;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+/*
+ * Author: Prasad
+ */
+class FindMaxTest {
+	/*
+	 * Test case to check maximum number is at first position
+	 */
+	@Test
+	public void maxNumberAtFirstPosition() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first integer number");	
+		Integer num1 = sc.nextInt();
+		System.out.println("Enter second integer number");
+		Integer num2 = sc.nextInt();
+		System.out.println("Enter third integer number");
+		Integer num3 = sc.nextInt();
+		Integer max = null;
+		//object
+		FindMax findMax = new FindMax();
+		int actualResult = findMax.findMaximumInteger(num1, num2, num3, max);
+		int expectedResult = num1;
+		Assert.assertEquals(expectedResult, actualResult);
+	}
+	
+	/*
+	 * Test case to check maximum number is at second position
+	 */
+	@Test
+	public void maxNumberAtSecondPosition() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first integer number");	
+		Integer num1 = sc.nextInt();
+		System.out.println("Enter second integer number");
+		Integer num2 = sc.nextInt();
+		System.out.println("Enter third integer number");
+		Integer num3 = sc.nextInt();
+		Integer max = null;
+		FindMax findMax = new FindMax();
+		int actualResult = findMax.findMaximumInteger(num1, num2, num3, max);
+		int expectedResult = num2;
+		Assert.assertEquals(expectedResult, actualResult);
+	}
+	
+	/*
+	 * Test case to check maximum number is at third position
+	 */
+	@Test
+	public void maxNumberAtThirdPosition() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first integer number");	
+		Integer num1 = sc.nextInt();
+		System.out.println("Enter second integer number");
+		Integer num2 = sc.nextInt();
+		System.out.println("Enter third integer number");
+		Integer num3 = sc.nextInt();
+		Integer max = null;
+		FindMax findMax = new FindMax();
+		int actualResult = findMax.findMaximumInteger(num1, num2, num3, max);
+		int expectedResult = num3;
+		Assert.assertEquals(expectedResult, actualResult);
+	}
+}
